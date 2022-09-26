cefdddcf667c46df62439a908afaf2558bb89193
Esau Lopez <56326583+elopez73@users.noreply.github.com>
2022-09-26 14:52:42 -0700

    56

--- CMS1500/Boxes/b1.js
+++ CMS1500/Boxes/b1.js
@@ -3,7 +3,7 @@ const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
 
-const setInfo = document.querySelectorAll('[data-info-target]') 
+const setInfo = document.querySelectorAll('[data-info-target]')
 const classBod = document.getElementsByClassName('.modal-body.active')
 
 
@@ -12,42 +12,45 @@ openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget)
     openModal(modal)
-    
+
     openModalButtons.forEach(button => {
       button.addEventListener('click', () => {
+
+
+
       const modal = document.querySelector(button.dataset.modalTarget)
       const info =  document.getElementById(button.dataset.infoTarget)
-      
+
       modal.textContent = info.textContent
-      console.log(modal)
-      console.log(info)
-        
-  })    
+
+      
+
   })
-  })    
   })
-  
+  })
+  })
+
 
 
 
 
 closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => { 
-    
-    
-    const modal = button.closest('.modal') 
-    
-    closeModal(modal)
-    
+  button.addEventListener('click', () => {
 
-  
+
+
+    const modal1 = button.closest('.modal')
+    closeModal(modal1)
+    const modal2 = document.getElementsByClassName('modal-body active')[0]
+    const info1 = "";
+    modal2.textContent = info1.textContent
   })
 })
- 
 
 
 
-  
+
+
 
 
 overlay.addEventListener('click', () => {
@@ -60,8 +63,8 @@ overlay.addEventListener('click', () => {
 
 function openModal(modal)
  {
-  
-  
+
+
   if (modal == null) return
   modal.classList.add('active')
   overlay.classList.add('active')
@@ -69,17 +72,11 @@ function openModal(modal)
 
 function closeModal(modal)
  {
-  
- 
+
+
   if (modal == null) return
-  
+
   modal.classList.remove('active')
   overlay.classList.remove('active')
-  
+
  }
-
-
-
-
-
-
