80083f5f013250fdf6dd06a482fc8ed1b2ecf42b
Esau Lopez <esaulopez14@gmail.com>
2022-11-13 12:26:18 -0700

    5

--- JavaS/index.js
+++ JavaS/index.js
@@ -6,36 +6,61 @@ const Content= document.getElementById("Content");
 const exit = document.querySelector(".Exit img");
 const Navheight =document.querySelector('.navbar').offsetHeight;
 
+
+
 document.documentElement.style.setProperty('--scroll-padding',Navheight+ -2 +"px");
 
-/*Boxes*/
+const sections = document.querySelectorAll('section');
+const navLi= document.querySelectorAll('  .navbar ul li')
 
 
-window.onclick = function(event) 
+window.addEventListener('scroll',()=>{
+        let current = '';
+        sections.forEach(section =>{
+        const sectionTop = section.offsetTop - 50;
+        const sectionHeight = section.clientHeight;
+        if ( scrollY >= sectionTop - sectionHeight/5){
+                current = section.getAttribute('id');
+        }
+        })
+
+        navLi.forEach( li => {
+            li.classList.remove('active');
+            if(li.classList.contains(current)){
+                li.classList.add('active')
+            }
+        })
+})
+
+var body = document.body,timer;
+window.addEventListener('scroll', function() {
+   clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+   timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },100);
+}, false);
+window.onclick = function(event)
 {
   if (event.target == exit) {
     modal.style.display = "none";
   }
 }
 function showFunction(pathmodalimage)
-{       
-        
+{
+
         var orgiginalSrc =  pathmodalimage;
         modalImg.src =`${orgiginalSrc}`
         modal.style.display = ("inline-flex");
 };
 function seturl(link)
-{       
+{
         EDI.setAttribute("href", link);
 }
 function settext(link)
-{       
+{
         Content.innerHTML = link;
         Content.setAttribute("")
 }
-
-
-
-  
-
-
