e3df1dcd0691eccda30d93c8bd751fe4cd8d1888
Esau Lopez <56326583+elopez73@users.noreply.github.com>
2022-11-16 15:25:28 -0700

    5569

--- CMS1500/Boxes/b1.js
+++ CMS1500/Boxes/b1.js
@@ -1,82 +1,59 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
 
+const setInfo = document.querySelectorAll("[data-info-target]");
+const classBod = document.getElementsByClassName(".modal-body.active");
 
-const setInfo = document.querySelectorAll('[data-info-target]')
-const classBod = document.getElementsByClassName('.modal-body.active')
+openModalButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const modal = document.querySelector(button.dataset.modalTarget);
+		openModal(modal);
+		openModalButtons.forEach((button) => {
+			button.addEventListener("click", () => {
+				const modal = document.querySelector(button.dataset.modalTarget);
+				const info = document.getElementById(button.dataset.infoTarget);
+        if(info != null)
+        {
+          modal.textContent = info.textContent;
+        }
+				
+			});
+		});
+	});
+});
 
+closeModalButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const modal1 = button.closest(".modal");
+		closeModal(modal1);
+		const modal2 = document.getElementsByClassName("modal-body active")[0];
+		const info1 = " ";
+		if(modal2 != null)
+		{
+			modal2.textContent = info1;
+		}
+    closeModal(modal2)
+	});
+});
 
+overlay.addEventListener("click", () => {
+	const modals = document.querySelectorAll(".modal.active");
+	modals.forEach((modal) => {
+		closeModal(modal);
+	});
+});
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-
-    openModalButtons.forEach(button => {
-      button.addEventListener('click', () => {
-
-
-
-      const modal = document.querySelector(button.dataset.modalTarget)
-      const info =  document.getElementById(button.dataset.infoTarget)
-
-      modal.textContent = info.textContent
-
-      
-
-  })
-  })
-  })
-  })
-
-
-
-
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-
-
-
-    const modal1 = button.closest('.modal')
-    closeModal(modal1)
-    const modal2 = document.getElementsByClassName('modal-body active')[0]
-    const info1 = "";
-    modal2.textContent = info1.textContent
-  })
-})
-
-
-
-
-
-
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-
-function openModal(modal)
- {
-
-
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+function openModal(modal) {
+	if (modal == null) return;
+	modal.classList.toggle("active");
+	overlay.classList.add("active");
+  console.log(modal);
 }
 
-function closeModal(modal)
- {
+function closeModal(modal) {
+	if (modal == null) return;
 
-
-  if (modal == null) return
-
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-
- }
+	modal.classList.remove("active");
+	overlay.classList.remove("active");
+}
